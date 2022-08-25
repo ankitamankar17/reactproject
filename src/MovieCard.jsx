14e8dd27fc19a110791fa805e3702083ef0f6e4c
unknown <ankitamankar1111@gmail.com>
2022-08-25 18:58:50 +0530

    Configure React app for deployment to GitHub Pages

---
+++ src/MovieCard.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+function MovieCard(props){
+    const{movie}=props
+    return(
+        <div  className="moviecard">
+      
+            <img src={movie.Poster} alt="poster" className="posterimage"></img>
+            <div>
+                <p className="cardtitle"><b>{movie.Title}</b></p>
+            </div>
+
+        
+        </div>
+    )
+}
+export default MovieCard
